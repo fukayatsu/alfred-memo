0c1c38d451b749e0f74053e08bededbbf63e1c15
fukayatsu <fukayatsu@gmail.com>
2014-01-07 22:15:41 +0900

    first commit

---
+++ memo.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+STORE_DIR = "/Users/fukayatsu/Dropbox/alfred-memo"
+EDITOR    = "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+FILE_EXT  = ".md"
+FileUtils.mkdir_p(STORE_DIR)
+
+date_str  = Time.now.strftime("%Y-%m-%d")
+time_str  = Time.now.strftime("%H:%M")
+file_path = "#{STORE_DIR}/#{date_str}#{FILE_EXT}"
+
+query     = ARGV[0]
+if query.length == 0
+  return system(EDITOR, '-n', '-a', STORE_DIR, file_path)
+end
+
+new_line = "#{time_str} #{query}"
+File.open(file_path, 'a') do |file|
+  file.puts new_line
+end
+
+puts "saved: #{new_line}"
